585eecf483c412ab1b227dc000a25c0546d16b29
Andre Basche <andre.basche@gmail.com>
2022-11-13 06:02:09 +0100

    Moved device out of module

--- jeelink/pca.py
+++ jeelink/pca.py
@@ -3,13 +3,13 @@ import logging
 import sys
 
 import serial_asyncio
-from jeelink.devices import PCADevice
 from jeelink.reader import PCAJeeLinkReader
 from serial.tools import list_ports
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG,
                     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 _LOGGER = logging.getLogger(__name__)
+
 
 class PCA:
     def __init__(self):
@@ -32,10 +32,10 @@ class PCA:
                                                                                    self._port, baudrate=self._baud)
         self._reader.register_callback(self._get_updates)
         await asyncio.sleep(5)
-        self.write("l")
-        self.write("v")
+        self._write("l")
+        self._write("v")
         # Turn off the blue LED
-        self.write("0a")
+        self._write("0a")
 
     @property
     def devices(self):
@@ -43,12 +43,20 @@ class PCA:
 
     def _get_updates(self, device_id, data=None, intern_number=0):
         if intern_number:
-            self.write(f"{intern_number}p")
+            self._write(f"{intern_number}p")
             return
         if device_id not in self._devices:
-            self._devices[device_id] = PCADevice(self, device_id)
-        self._devices[device_id].get_updates(data)
+            self._devices[device_id] = []
+        for callback in self._devices[device_id]:
+            callback(data)
 
-    def write(self, text):
+    def _write(self, text):
         _LOGGER.debug(f"Write - {text}")
         self._writer.write(text.encode())
+
+    def send_command(self, device_id, channel_id, command, data):
+        address = f"{int(device_id[:3])},{int(device_id[3:6])},{int(device_id[6:])}"
+        self._write(f"{channel_id},{command},{address},{data},255,255,255,255s")
+
+    def register_callback(self, device_id, callback):
+        self._devices[device_id].append(callback)

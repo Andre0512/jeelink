c02eaa2fdef3a8fa0d9fca7032cf2e65c2af0b93
Andre Basche <andre.basche@gmail.com>
2023-01-10 18:18:45 +0100

    Deploy with github action

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="jeelink",
-    version="0.2.0",
+    version="0.2.1",
     author="Andre Basche",
     description="Python interface for jeelink sketches",
     long_description=long_description,
